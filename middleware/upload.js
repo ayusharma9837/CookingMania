357eaa0e9e030ec61fccc5e7c823c8f8f5f23451
Ayush Sharma <117968490+ayusharma9837@users.noreply.github.com>
2025-11-26 17:58:43 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "/tmp");
   },
   filename: (req, file, cb) => {
     cb(
@@ -26,3 +26,4 @@ const upload = multer({
 });
 
 module.exports = upload.single("image");
+
